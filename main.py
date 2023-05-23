78d5aa7d2c107362a29189fb5d873e2edeb58081
Danny Bui <Dannybui5155@gmail.com>
2023-05-23 14:29:34 -0500

    Implemented webapp for plotting data and webscraping temperature

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+import selectorlib
+import os
+import time
+from datetime import datetime
+
+
+URL = "http://programmer100.pythonanywhere.com/"
+
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url)
+    source = response.text
+    return source
+
+
+def extract(source):
+    """Extract the selector"""
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["temperature"]
+    return value
+
+
+def store(extracted):
+    """Stores the event in a txt file"""
+    now = datetime.now()
+    content = now.strftime("%y-%m-%d-%H-%M-%S") + "," + extracted
+    with open("data.txt", "a") as file:
+        file.write(content + "\n")
+
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+
+    store(extracted)
---
+++ webapp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import plotly.express as px
+import pandas
+
+st.title("Average World Temperature")
+
+df = pandas.read_csv("data.txt")
+
+figure = px.line(x=df["date"], y=df["temperature"],
+                 labels={"x": "Date", "y": "Temperature (C)"})
+
+st.plotly_chart(figure)
